1b1c7e892e6d2a4c419a54bfce89fb3372c65ec6
grf <1600588658@qq.com>
2021-03-28 23:02:32 +0800

    这是2009_git progect

---
+++ src/main/java/com/example/git_2009/Git2009Application.java
@@ -0,0 +1,13 @@
+package com.example.git_2009;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2009Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2009Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git_2009/Git2009ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_2009;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git2009ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
